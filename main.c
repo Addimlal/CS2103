240a801d9076473fe8221d88ae5ddb64331923ab
Fernandos <worldwide@online.de>
2011-10-17 16:18:04 +0200

    abgeschlossen: void interpretiere(Knoten *baum)

--- main.c
+++ main.c
@@ -15,11 +15,49 @@
 
 
 void zeigeBaum(Knoten *baum) {
-  /******************************/
-  /* Das muessen Sie schreiben! */
-  /******************************/
-  printf("Hier sollte die von Ihnen programmierte ");
-  printf("Ausgabe des Programmbaumes stehen!");
+  switch( baum->typ )
+  {
+	  case KNOTEN_FOLGE:
+	      zeigeBaum(baum->u.folge.anweisung1);
+	      printf(" ; ");
+	      zeigeBaum(baum->u.folge.anweisung2);
+	      break;
+	  case KNOTEN_ZUWEISUNG:
+	      printf("%s :=  ", baum->u.zuweisung.name);
+	      zeigeBaum(baum->u.zuweisung.ausdruck);
+	      break;
+	  case KNOTEN_AUSGABE:
+	      printf("Zeige(");
+	      zeigeBaum(baum->u.ausgabe.ausdruck);
+	      printf(")");
+	      break;
+	  case KNOTEN_LITERAL:
+	      printf("%i", baum->u.literal.wert);
+	      break;
+	  case KNOTEN_VARIABLE:
+	      printf("%s", baum->u.variable.name);
+	      break;
+	  case KNOTEN_SUMME:
+	      zeigeBaum(baum->u.summe.ausdruck1);
+	      printf(" + ");
+	      zeigeBaum(baum->u.summe.ausdruck2);
+	      break;
+	  case KNOTEN_DIFFERENZ:
+	      zeigeBaum(baum->u.differenz.ausdruck1);
+	      printf(" - ");
+	      zeigeBaum(baum->u.differenz.ausdruck2);
+	      break;
+	  case KNOTEN_PRODUKT:
+	      zeigeBaum(baum->u.produkt.ausdruck1);
+	      printf(" * ");
+	      zeigeBaum(baum->u.produkt.ausdruck2);
+	      break;
+	  case KNOTEN_QUOTIENT:
+	      zeigeBaum(baum->u.quotient.ausdruck1);
+	      printf(" / ");
+	      zeigeBaum(baum->u.quotient.ausdruck2);
+	      break;
+  }
 }
 
 
