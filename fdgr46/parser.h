748bd2b2de7ae007f8e9e6802c0a36df7fc30137
Fernandos <worldwide@online.de>
2011-11-24 16:05:02 +0100

    * almost our release version, missing team.txt

---
+++ fdgr46/parser.h
@@ -0,0 +1,15 @@
+/*
+ * parser.h -- SPL parser interface
+ */
+
+
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+extern Absyn *progTree;
+
+int yyparse(void);
+void yyerror(char *msg);
+
+
+#endif /* _PARSER_H_ */
